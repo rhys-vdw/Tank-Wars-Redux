35d363447368b0b17625dc3a0222adf5c6ad8f24
Rhys van der Waerden <rhys.vdw@gmail.com>
2011-06-05 02:13:58 +1000

    wrote main battle update() method, just needs some placeholder methods for all the different states

--- BattleGameState.java
+++ BattleGameState.java
@@ -10,23 +10,27 @@ public class BattleGameState extends GameState {
   private int dirtColor;
   private DirtGenerator dirtGenerator;
   private ArrayList<Tank> tanks;
-
-  /* TODO: consider using an Iterator<Tank> in conjunction with a Tank currentTank */
-  private int currentTank;
+  /* records whether the last crumble actually did anything */
+  private boolean crumbleOccurred;
+  /* TODO: consider using an Iterator<Tank> in conjunction with a Tank tankCurrent */
+  /* the tank which is currently being updated (falling, acting, dying etc.) */
+  private int tankCurrent;
+  /* the next tank to have a turn */
+  private int tankTurn;
 
   private final static int BACKGROUND_COLOR = 0xFF382428;
   private final static int DEFAULT_DIRT_COLOR = 0xFF00A800;
 
-  private final static boolean FALLING = true;
-  private final static boolean LANDED = false;
+  private final static boolean FINISHED = true;
+  private final static boolean NOT_FINISHED = false;
 
   private final static boolean DAMAGE = true;
   private final static boolean NO_DAMAGE = false;
 
-  private final static float FALL_SPEED = 200.0f;
+  private final static float FALL_SPEED = 700.0f;
 
   /* TODO: replace with GameOptions class */
-  private final static int PLAYERS = 2;
+  private final static int PLAYERS = 4;
 
   /** The state of the battle at present. */
   private enum BattleState {
@@ -35,11 +39,13 @@ public class BattleGameState extends GameState {
       /** player is setting up a shot */
       PlayerTurn,
       /** updating missile movement/weapon behaviour */
-      ResolvingShot,
+      ResolvingTurn,
       /** dropping suspended dirt */
-      CollapsingDirt,
+      CrumblingDirt,
       /** dropping tanks that are not supported by dirt */
-      DroppingTanks
+      DroppingTanks,
+      /** removing dead tanks from game */
+      DestroyingDead
   }
   private BattleState battleState;
 
@@ -60,7 +66,7 @@ public class BattleGameState extends GameState {
     /* load tanks */
     loadTanks();
     this.battleState = BattleState.LoweringTanks;
-    this.currentTank = 0;
+    this.tankTurn = 0;
   }
 
   @Override
@@ -71,12 +77,45 @@ public class BattleGameState extends GameState {
     /* check battle state and act accordingly */
     switch (this.battleState) {
       case LoweringTanks:
-        if (lowerTank(this.tanks.get(currentTank), NO_DAMAGE, deltaTime) == LANDED) {
-          if (this.currentTank == this.tanks.size() - 1) {
-            this.battleState = BattleState.PlayerTurn;
+        this.tankCurrent = 0;
+        if (dropTanks(NO_DAMAGE, deltaTime) == FINISHED) {
+          this.battleState = BattleState.PlayerTurn;
+        }
+        break;
+      case PlayerTurn:
+        if (tanks[tankTurn].update(deltaTime) == FINISHED) {
+          this.tankTurn = (this.tankTurn == this.tanks.size() - 1) ?
+              this.tankTurn + 1 : 0;
+          this.battleState = BattleState.ResolvingTurn;
+        }
+        break;
+      case ResolvingTurn:
+        if (updateWeapon() == FINISHED) {
+          this.battleState = DestroyingDead;
+        }
+        break;
+      case CrumblingDirt:
+        this.crumbleOccurred = false;
+        if (crumbleDirt(this.lastImpact, deltaTime) == FINISHED) {
+          if (this.crumbleOccured) {
+            this.battleState = DroppingTanks;
           } else {
-            currentTank++;
+            this.battleState = PlayerTurn;
           }
+        } else {
+          this.crumbleOccurred = true;
+        }
+        break;
+      case DroppingTanks:
+        this.tankCurrent = 0;
+        if (dropTanks(DAMAGE, deltaTime) == FINISHED) {
+          this.battleState = BattleState.DestroyingDead;
+        }
+        break;
+      case DestroyingDead:
+        this.tankCurrent = 0;
+        if (destroyDead(deltaTime) == FINISHED) {
+          this.battleState = CrumblingDirt;
         }
         break;
     }
@@ -125,26 +164,39 @@ public class BattleGameState extends GameState {
     int tankSeperation = this.mapWidth / (PLAYERS + 1);
 
     for (int i = 0; i < PLAYERS; i++) {
-      this.tanks.add(new Tank(game, null, tankSeperation * (i + 1), 10, 0xFFFF0000));
+      this.tanks.add(new Tank(game, null, tankSeperation * (i + 1), 0, 0xFFFF0000));
+    }
+  }
+
+  /** Drop all tanks to earth.
+   *  @param deltaTime  delta time in s
+   *  @return           true (FINISHED) or false (NOT_FINISHED)
+   */
+  private boolean dropTanks(boolean damage, float deltaTime) {
+    if (lowerTank(this.tanks.get(tankCurrent), damage, deltaTime) == LANDED) {
+      if (this.tankCurrent == this.tanks.size() - 1) {
+        return FINISHED;
+      this.tankCurrent++;
+      return NOT_FINISHED;
     }
   }
 
   /** Drop a tank until it can go no further down.
    *  @param tank   the tank to be dropped
-   *  @return       true (FALLING) if falling, false (LANDED) if done
+   *  @return       true (FINISHED) or false (NOT_FINISHED)
    */
-  private boolean lowerTank(Tank tank, boolean damage, float deltaTime) {
+  private boolean dropTank(Tank tank, boolean damage, float deltaTime) {
     /* TODO: more advanced collision algorithm... Consider attaching colliders
      *       to tanks/missiles */
     float distance = FALL_SPEED * deltaTime;
-    boolean landed = FALLING;
+    boolean finished = NOT_FINISHED;
     /* check if movement will hit dirt or the bottom of the level */
     for (int i = 0;
         (i < (int) distance) && (i + (int) tank.getY() < mapHeight);
         i++) {
       if (dirt[(int) tank.getX()][(int) tank.getY() + i]) {
         distance = i;
-        landed = LANDED;
+        finished = FINISHED;
         break;
       }
     }
@@ -152,6 +204,6 @@ public class BattleGameState extends GameState {
     tank.move(0.0f, distance);
     /* deal falling damage */
     if (damage) tank.damage(distance);
-    return landed;
+    return finished;
   }
 }
